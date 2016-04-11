2d4ac4dbd9ce8ee44faba353ef1be9c75ee8aa5a
Marta Rożek <mr360953@students.mimuw.edu.pl>
2016-04-11 14:01:50 +0200

    Improved reports models, added admin

--- caffe/reports/admin.py
+++ caffe/reports/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, Category, Unit, Report, FullProduct
 
-# Register your models here.
+admin.site.register(Report)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Unit)
+admin.site.register(FullProduct)
--- caffe/reports/models.py
+++ caffe/reports/models.py
@@ -1,14 +1,22 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
 
 
 class Report(models.Model):
     date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return 'Report created: {:%Y-%m-%d %H:%M} '.format(self.date)
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100,
                             unique=True,
                             )
+
+    def __str__(self):
+        return '{}'.format(self.name)
 
 
 class Product(models.Model):
@@ -22,18 +30,28 @@ class Product(models.Model):
                              on_delete=models.CASCADE,
                              )
 
+    def __str__(self):
+        return '{}'.format(self.name)
+
 
 class Unit(models.Model):
     name = models.CharField(max_length=100,
                             unique=True,
                             )
 
+    def __str__(self):
+        return '{}'.format(self.name)
+
 
 class FullProduct(models.Model):
     product = models.ForeignKey('Product',
                                 on_delete=models.CASCADE,
                                 )
-    amount = models.PositiveIntegerField()
+    amount = models.FloatField(validators=[MinValueValidator(0)])
+
     report = models.ForeignKey('Report',
                                on_delete=models.CASCADE,
                                )
+
+    def __str__(self):
+        return '{0}, {1:g} {2}'.format(self.product, self.amount, self.product.unit)

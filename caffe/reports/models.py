ce52e1c90e6623011b6f90910f94553fa8166cf0
VirrageS <virrages@gmail.com>
2016-04-11 15:57:18 +0200

    Minor style update

--- caffe/reports/models.py
+++ caffe/reports/models.py
@@ -11,47 +11,36 @@ class Report(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100,
-                            unique=True,
-                            )
+    name = models.CharField(max_length=100, unique=True,)
 
     def __str__(self):
         return '{}'.format(self.name)
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100,
-                            unique=True,
-                            )
-    category = models.ForeignKey('Category',
-                                 on_delete=models.CASCADE,
-                                 )
-    unit = models.ForeignKey('Unit',
-                             on_delete=models.CASCADE,
-                             )
+    name = models.CharField(max_length=100, unique=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    unit = models.ForeignKey('Unit', on_delete=models.CASCADE)
 
     def __str__(self):
         return '{}'.format(self.name)
 
 
 class Unit(models.Model):
-    name = models.CharField(max_length=100,
-                            unique=True,
-                            )
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return '{}'.format(self.name)
 
 
 class FullProduct(models.Model):
-    product = models.ForeignKey('Product',
-                                on_delete=models.CASCADE,
-                                )
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
     amount = models.FloatField(validators=[MinValueValidator(0)])
-
-    report = models.ForeignKey('Report',
-                               on_delete=models.CASCADE,
-                               )
+    report = models.ForeignKey('Report', on_delete=models.CASCADE)
 
     def __str__(self):
-        return '{0}, {1:g} {2}'.format(self.product, self.amount, self.product.unit)
+        return '{0}, {1:g} {2}'.format(
+            self.product,
+            self.amount,
+            self.product.unit
+        )

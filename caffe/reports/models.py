7d37bed0ac5fedfa0ae4e770aad051bb10ef7e7a
Marta Rożek <mr360953@students.mimuw.edu.pl>
2016-04-11 12:02:53 +0200

    Added Report, Category, Product, Unit, FullProduct classes

--- caffe/reports/models.py
+++ caffe/reports/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Report(models.Model):
+    date = models.DateTimeField(auto_now=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100,
+                            unique=True,
+                            )
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100,
+                            unique=True,
+                            )
+    category = models.ForeignKey('Category',
+                                 on_delete=models.CASCADE,
+                                 )
+    unit = models.ForeignKey('Unit',
+                             on_delete=models.CASCADE,
+                             )
+
+
+class Unit(models.Model):
+    name = models.CharField(max_length=100,
+                            unique=True,
+                            )
+
+
+class FullProduct(models.Model):
+    product = models.ForeignKey('Product',
+                                on_delete=models.CASCADE,
+                                )
+    amount = models.PositiveIntegerField()
+    report = models.ForeignKey('Report',
+                               on_delete=models.CASCADE,
+                               )

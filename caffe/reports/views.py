1bb62f3a5de90aa9800dd91cc07b66f017a87cd7
VirrageS <virrages@gmail.com>
2016-04-15 10:56:35 +0200

    Removed unnecessary code

--- caffe/reports/views.py
+++ caffe/reports/views.py
@@ -6,27 +6,6 @@ from .models import Report, FullProduct, Product, Unit, Category
 from .forms import ReportForm, ProductForm
 from .forms import FullProductForm, UnitForm, CategoryForm
 
-# url(r'^new/category/$', reports_new_category, name='reports_new_category'),
-# url(r'^edit/category/(?P<category_id>\d+)/$',
-# 	reports_edit_category, name='reports_edit_category'),
-#
-# url(r'^new/unit/$', reports_new_unit, name='reports_new_unit'),
-# url(r'^edit/unit/(?P<unit_id>\d+)/$',
-# 	reports_edit_unit, name='reports_edit_unit'),
-#
-# url(r'^new/product/$', reports_new_product, name='reports_new_product'),
-# url(r'^edit/product/(?P<product_id>\d+)/$',
-# 	reports_edit_product, name='reports_edit_product'),
-#
-# url(r'^new/fullproduct/$',
-# 	reports_new_fullproduct, name='reports_new_fullproduct'),
-# url(r'^edit/fullproduct/(?P<fullproduct_id>\d+)/$',
-# 	reports_edit_fullproduct, name='reports_edit_fullproduct'),
-#
-# url(r'^new/report/$', reports_new_report, name='reports_new_report'),
-# url(r'^edit/report/(?P<report_id>\d+)/$',
-# 	reports_edit_report, name='reports_edit_report'),
-
 def reports_new_category(request):
     elements = []
     form = CategoryForm()

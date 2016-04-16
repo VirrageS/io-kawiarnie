3e8f0f5429b61ce08c3f3bc8b2836be67d530452
VirrageS <virrages@gmail.com>
2016-04-16 13:15:52 +0200

    Fix: postgresql docker fixes

--- start-web.sh
+++ start-web.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-cd caffe
+cd caffe;
+sleep 5;
 su -m docker -c "python3 manage.py migrate"
 su -m docker -c "python3 manage.py runserver 0.0.0.0:8000"
+
+# uwsgi --http :8000 --module caffe.wsgi

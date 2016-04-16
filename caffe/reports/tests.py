fd85f47fc733141abc885a9b48b39c3fa5ba8caf
tknopik <tomasz.knopik@gmail.com>
2016-04-16 14:47:37 +0200

    next forms tests

--- caffe/reports/tests.py
+++ caffe/reports/tests.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from django.db import transaction
 from django import forms
 
+import unittest
+
 from datetime import datetime
 
 from .models import \
@@ -22,326 +24,418 @@ from .forms import \
 ############## MODELS TESTS #################
 
 class ReportModelTest(TestCase):
-  def setUp(self):
-    Report.objects.create(id = 1)
-    Report.objects.create(id = 2)
+    def setUp(self):
+        Report.objects.create(id = 1)
+        Report.objects.create(id = 2)
 
-  def test_created_on(self):
-    now = timezone.now()
-    r1 = Report.objects.get(id = 1)
-    #self.assertTrue(datetime.now() - r1.created_on > 0)
-    #self.assertTrue(now < r1.created_on)
+    def test_created_on(self):
+        now = timezone.now()
+        r1 = Report.objects.get(id = 1)
+        #self.assertTrue(datetime.now() - r1.created_on > 0)
+        #self.assertTrue(now < r1.created_on)
 
 class CategoryModelTest(TestCase):
-  def setUp(self):    
-    Category.objects.create(name = "first")
-    Category.objects.create(name = "second")
+    def setUp(self):    
+        Category.objects.create(name = "first")
+        Category.objects.create(name = "second")
 
-  def test_category_name(self):
-    c1 = Category(name = "first")
-    c2 = Category(name = "second")
+    def test_category_name(self):
+        c1 = Category(name = "first")
+        c2 = Category(name = "second")
 
-    self.assertTrue(c1.name != c2.name)
-    self.assertEqual(c1.name, "first")
-    self.assertEqual(c2.name, "second")
-    self.assertRaises(Exception, Category.objects.create, name = "first")
-    self.assertRaises(Exception, Category.objects.create, name = "second")
+        self.assertTrue(c1.name != c2.name)
+        self.assertEqual(c1.name, "first")
+        self.assertEqual(c2.name, "second")
+        self.assertRaises(Exception, Category.objects.create, name = "first")
+        self.assertRaises(Exception, Category.objects.create, name = "second")
 
 class UnitModelTest(TestCase):
-  def setUp(self):
-    Unit.objects.create(name = "gram")
-    Unit.objects.create(name = "liter")
+    def setUp(self):
+        Unit.objects.create(name = "gram")
+        Unit.objects.create(name = "liter")
 
-  def test_unit(self):
-    u_gram = Unit.objects.get(name = "gram")
-    u_liter = Unit.objects.get(name = "liter")
-    u_gram2 = Unit.objects.get(name = "gram")
+    def test_unit(self):
+        u_gram = Unit.objects.get(name = "gram")
+        u_liter = Unit.objects.get(name = "liter")
+        u_gram2 = Unit.objects.get(name = "gram")
 
-    self.assertEqual(u_gram.id, u_gram2.id)
-    self.assertRaises(Exception, Unit.objects.create, name = "liter")
+        self.assertEqual(u_gram.id, u_gram2.id)
+        self.assertRaises(Exception, Unit.objects.create, name = "liter")
 
 
 
 class ProductModelTest(TestCase):
-  def setUp(self):    
-    Category.objects.create(name = "first")
-    Category.objects.create(name = "second")
+    def setUp(self):    
+        Category.objects.create(name = "first")
+        Category.objects.create(name = "second")
 
-    Unit.objects.create(name = "gram")
-    Unit.objects.create(name = "liter")   
+        Unit.objects.create(name = "gram")
+        Unit.objects.create(name = "liter")   
 
-  def test_product(self):
-    first_cat = Category.objects.get(name = "first")
-    second_cat = Category.objects.get(name = "second")
+    def test_product(self):
+        first_cat = Category.objects.get(name = "first")
+        second_cat = Category.objects.get(name = "second")
 
-    gram = Unit.objects.get(name = "gram")
-    liter = Unit.objects.get(name = "liter")
+        gram = Unit.objects.get(name = "gram")
+        liter = Unit.objects.get(name = "liter")
 
-    p1 = Product.objects.create(
-      name = "product1", 
-      category = first_cat, 
-      unit = gram
-    )
-    p2 = Product.objects.create(
-      name = "product2",
-      category = first_cat,
-      unit = liter
-    )
-    p3 = Product.objects.create(
-      name = "product3",
-      category = second_cat,
-      unit = gram
-    )
-    p4 = Product.objects.create(
-      name = "product4",
-      category = second_cat,
-      unit = liter
-    )
+        p1 = Product.objects.create(
+            name = "product1", 
+            category = first_cat, 
+            unit = gram
+        )
+        p2 = Product.objects.create(
+            name = "product2",
+            category = first_cat,
+            unit = liter
+        )
+        p3 = Product.objects.create(
+            name = "product3",
+            category = second_cat,
+            unit = gram
+        )
+        p4 = Product.objects.create(
+            name = "product4",
+            category = second_cat,
+            unit = liter
+        )
 
 
-    self.assertEqual(first_cat.product_set.count(), 2)
-    p2.delete()
-    self.assertEqual(first_cat.product_set.count(), 1)
-    p3.category = first_cat
-    p3.save()
-    self.assertEqual(first_cat.product_set.count(), 2)
-    self.assertEqual(second_cat.product_set.count(), 1)
-    p3.category = second_cat
-    p3.save()
+        self.assertEqual(first_cat.product_set.count(), 2)
+        p2.delete()
+        self.assertEqual(first_cat.product_set.count(), 1)
+        p3.category = first_cat
+        p3.save()
+        self.assertEqual(first_cat.product_set.count(), 2)
+        self.assertEqual(second_cat.product_set.count(), 1)
+        p3.category = second_cat
+        p3.save()
 
-    self.assertEqual(gram.product_set.count(), 2)
-    p3.delete()
-    self.assertEqual(gram.product_set.count(), 1)
+        self.assertEqual(gram.product_set.count(), 2)
+        p3.delete()
+        self.assertEqual(gram.product_set.count(), 1)
 
-    self.assertRaises(
-      Exception,
-      Product.objects.create,
-      name = "product1",
-      category = first_cat,
-      unit = gram
-    )
+        self.assertRaises(
+            Exception,
+            Product.objects.create,
+            name = "product1",
+            category = first_cat,
+            unit = gram
+        )
 
 class FullProductModelTest(TestCase):
-  def setUp(self):      
-    first_cat = Category.objects.create(name = "first")
-    second_cat = Category.objects.create(name = "second")
+    def setUp(self):      
+        first_cat = Category.objects.create(name = "first")
+        second_cat = Category.objects.create(name = "second")
 
-    gram = Unit.objects.create(name = "gram")
-    liter = Unit.objects.create(name = "liter")   
+        gram = Unit.objects.create(name = "gram")
+        liter = Unit.objects.create(name = "liter")   
 
-    Product.objects.create(
-      name = "product1", 
-      category = first_cat, 
-      unit = gram
-    )
-    Product.objects.create(
-      name = "product2",
-      category = first_cat,
-      unit = liter
-    )
-    Product.objects.create(
-      name = "product3",
-      category = second_cat,
-      unit = gram
-    )
-    Product.objects.create(
-      name = "product4",
-      category = second_cat,
-      unit = liter
-    )
+        Product.objects.create(
+            name = "product1", 
+            category = first_cat, 
+            unit = gram
+        )
+        Product.objects.create(
+            name = "product2",
+            category = first_cat,
+            unit = liter
+        )
+        Product.objects.create(
+            name = "product3",
+            category = second_cat,
+            unit = gram
+        )
+        Product.objects.create(
+            name = "product4",
+            category = second_cat,
+            unit = liter
+        )
 
-  def test_fullProduct(self):
-    p1 = Product.objects.get(name = "product1")
-    p2 = Product.objects.get(name = "product2")
-    p3 = Product.objects.get(name = "product3")
-    p4 = Product.objects.get(name = "product4")
+    def test_fullProduct(self):
+        p1 = Product.objects.get(name = "product1")
+        p2 = Product.objects.get(name = "product2")
+        p3 = Product.objects.get(name = "product3")
+        p4 = Product.objects.get(name = "product4")
 
-    r1 = Report.objects.create(id = 1)
-    r2 = Report.objects.create(id = 2)
+        r1 = Report.objects.create(id = 1)
+        r2 = Report.objects.create(id = 2)
 
-    fp1 = FullProduct.objects.create(
-      product = p1,
-      amount = 10,
-      report = r1
-    )
+        fp1 = FullProduct.objects.create(
+            product = p1,
+            amount = 10,
+            report = r1
+        )
 
-    fp2 = FullProduct.objects.create(
-      product = p2,
-      amount = 100,
-      report = r1
-    )
+        fp2 = FullProduct.objects.create(
+            product = p2,
+            amount = 100,
+            report = r1
+        )
 
-    fp3 = FullProduct.objects.create(
-      product = p3,
-      amount = 0,
-      report = r2
-    )
+        fp3 = FullProduct.objects.create(
+            product = p3,
+            amount = 0,
+            report = r2
+        )
 
-    fp4 = FullProduct.objects.create(
-      product = p4,
-      amount = 1000,
-      report = r2
-    )
+        fp4 = FullProduct.objects.create(
+            product = p4,
+            amount = 1000,
+            report = r2
+        )
 
-    self.assertEqual(fp1.product.name, "product1")      
-    self.assertEqual(fp2.amount, 100)
-    self.assertEqual(fp2.report.id, r1.id)
+        self.assertEqual(fp1.product.name, "product1")      
+        self.assertEqual(fp2.amount, 100)
+        self.assertEqual(fp2.report.id, r1.id)
 
-    self.assertEqual(r2.full_products.count(), 2)
-    fp4.delete()
-    self.assertEqual(r2.full_products.count(), 1)
+        self.assertEqual(r2.full_products.count(), 2)
+        fp4.delete()
+        self.assertEqual(r2.full_products.count(), 1)
 
-    self.assertEqual(r1.full_products.count(), 2)
-    p2.delete()
-    self.assertEqual(r1.full_products.count(), 1)
+        self.assertEqual(r1.full_products.count(), 2)
+        p2.delete()
+        self.assertEqual(r1.full_products.count(), 1)
 
-    fp1.amount = -1
+        fp1.amount = -1
 
-    #this assertions should pass, but they dont
-    '''
-    self.assertRaises(Exception, fp1.save)
-    self.assertRaises(
-      Exception, 
-      FullProduct.objects.create,
-      product = p1,
-      amount = 50,
-      report = r1
-    )
-    '''
+        #this assertions should pass, but they dont
+        '''
+        self.assertRaises(Exception, fp1.save)
+        #p1 already exists in previous FullProduct
+        self.assertRaises(
+            Exception, 
+            FullProduct.objects.create,
+            product = p1,
+            amount = 50,
+            report = r1
+        )
+        '''
     
-################### FORMS TESTS ###########################
+########################## FORMS TESTS ###########################
 
 class CategoryFormTest(TestCase):
 
-  def test_category(self):
-    form_incorrect = CategoryForm({
-      'name':''
-    })
+    def test_category(self):
+        form_incorrect = CategoryForm({
+            'name':''
+        })
 
-    self.assertFalse(form_incorrect.is_valid())
+        self.assertFalse(form_incorrect.is_valid())
 
-    form_incorrect = CategoryForm({
-      'no_such':'field'
-    })
+        form_incorrect = CategoryForm({
+            'no_such':'field'
+        })
 
-    self.assertFalse(form_incorrect.is_valid())
+        self.assertFalse(form_incorrect.is_valid())
 
-    form_correct = CategoryForm({
-      'name':'Category is correct'
-    })
+        form_correct = CategoryForm({
+            'name':'Category is correct'
+        })
 
-    self.assertTrue(form_correct.is_valid())
+        self.assertTrue(form_correct.is_valid())
 
-    form_correct = CategoryForm({
-      'name':'This.is.correct123!@#$%"^&"*():?>M'
-    })
+        form_correct = CategoryForm({
+            'name':'This.is.correct123!@#$%"^&"*():?>M'
+        })
 
-    self.assertTrue(form_correct.is_valid())
+        self.assertTrue(form_correct.is_valid())
 
 
 class FullProductFormTest(TestCase):
 
-  def setUp(self):
-    first_cat = Category.objects.create(name = "first")
-    second_cat = Category.objects.create(name = "second")
+    def setUp(self):
+        first_cat = Category.objects.create(name = "first")
+        second_cat = Category.objects.create(name = "second")
 
-    gram = Unit.objects.create(name = "gram")
-    liter = Unit.objects.create(name = "liter")   
+        gram = Unit.objects.create(name = "gram")
+        liter = Unit.objects.create(name = "liter")   
 
-    Product.objects.create(
-      name = "product1", 
-      category = first_cat, 
-      unit = gram
-    )
+        Product.objects.create(
+            name = "product1", 
+            category = first_cat, 
+            unit = gram
+        )
 
-    Product.objects.create(
-      name = "product2", 
-      category = second_cat, 
-      unit = liter
-    )
+        Product.objects.create(
+            name = "product2", 
+            category = second_cat, 
+            unit = liter
+        )
 
-  def test_full_product(self):
-    p1 = Product.objects.get(name = "product1")
-    p2 = Product.objects.get(name = "product2")
+    def test_full_product(self):
+        p1 = Product.objects.get(name = "product1")
+        p2 = Product.objects.get(name = "product2")
 
-    form_correct = FullProductForm({
-      'product':p1.id,
-      'amount':10
-    })
+        form_correct = FullProductForm({
+            'product':p1.id,
+            'amount':10
+        })
 
-    self.assertTrue(form_correct.is_valid())
+        self.assertTrue(form_correct.is_valid())
 
-    form_correct = FullProductForm({
-      'product':p2.id,
-      'amount':10000000
-    })
+        form_correct = FullProductForm({
+            'product':p2.id,
+            'amount':10000000
+        })
 
-    self.assertTrue(form_correct.is_valid())
+        self.assertTrue(form_correct.is_valid())
 
-    p2.delete()
+        p2.delete()
 
-    form_incorrect = FullProductForm({
-      'product':p2.id,
-      'amount':10
-    })
+        form_incorrect = FullProductForm({
+            'product':p2.id,
+            'amount':10
+        })
 
-    self.assertFalse(form_incorrect.is_valid())
+        self.assertFalse(form_incorrect.is_valid())
 
-    form_incorrect = FullProductForm({
-      'product':'',
-      'amount':10
-    })
+        form_incorrect = FullProductForm({
+            'product':'',
+            'amount':10
+        })
 
-    self.assertFalse(form_incorrect.is_valid())
+        self.assertFalse(form_incorrect.is_valid())
 
-    form_incorrect = FullProductForm({
-      'product':'Product',
-      'amount':-10
-    })
+        '''
+        form_incorrect = FullProductForm({
+            'product':'1',
+            'amount':10
+        })
 
-    self.assertFalse(form_incorrect.is_valid())
+        self.assertFalse(form_incorrect.is_valid())
+        '''
 
-    form_incorrect = FullProductForm({
-      'no_such':'field'
-    })
+        form_incorrect = FullProductForm({
+            'product':p1.id,
+            'amount':-10
+        })
 
-    self.assertFalse(form_incorrect.is_valid())
+        self.assertFalse(form_incorrect.is_valid())
 
-    form_incorrect = FullProductForm({
-      'product':1231,
-      'amount':100
-    })
+        form_incorrect = FullProductForm({
+            'no_such':'field'
+        })
 
-    self.assertFalse(form_incorrect.is_valid())
+        self.assertFalse(form_incorrect.is_valid())
+
+        form_incorrect = FullProductForm({
+            'product':1231,
+            'amount':100
+        })
+
+        self.assertFalse(form_incorrect.is_valid())
     
+class UnitFormTest(TestCase):
 
-#to fix, smthg wrong
-'''
-class UnitForm(TestCase):
-
-  def test_unit(self):
+    def test_unit_form(self):
     
-    form_incorrect = UnitForm({
-      'name':'aasd'
-    })
+        form_correct = UnitForm({
+            "name" : "correct"
+        })
 
-    self.assertFalse(form_incorrect.is_valid())
+        self.assertTrue(form_correct.is_valid())
 
-    form_incorrect = UnitForm({
-      'no_such':'field'
-    })
+        form_incorrect = UnitForm({
+            'no_such':'field'
+        })
 
-    self.assertFalse(form_incorrect.is_valid())
+        self.assertFalse(form_incorrect.is_valid())
 
-    form_correct = UnitForm({
-      'name':'Category is correct'
-    })
+        form_correct = UnitForm({
+            'name':'Category is correct'
+        })
 
-    self.assertTrue(form_correct.is_valid())
+        self.assertTrue(form_correct.is_valid())
 
-    form_correct = UnitForm({
-      'name':'This.is.correct123!@#$%"^&"*():?>M'
-    })
+        form_correct = UnitForm({
+            'name':'This.is.correct123!@#$%"^&"*():?>M'
+        })
 
-    self.assertTrue(form_correct.is_valid())
-    '''
+        self.assertTrue(form_correct.is_valid())
+
+class ProductFormTest(TestCase):
+    def setUp(self):            
+        Category.objects.create(name = "first")
+        Category.objects.create(name = "second")
+
+        Unit.objects.create(name = "gram")
+        Unit.objects.create(name = "liter")
+
+    def test_product_form(self):
+        first_cat = Category.objects.get(name = "first")
+        second_cat = Category.objects.get(name = "second")
+
+        gram = Unit.objects.get(name = "gram")
+        liter = Unit.objects.get(name = "liter")
+
+        form_correct = ProductForm({
+            'name':'Some correct product',
+            'category':first_cat.id,
+            'unit':gram.id
+        })
+
+        self.assertTrue(form_correct.is_valid())
+
+        form_correct = ProductForm({
+            'name':'Some correct product',
+            'category':second_cat.id,
+            'unit':liter.id
+        })
+
+        self.assertTrue(form_correct.is_valid())
+
+        form_incorrect = ProductForm({
+            'name':"This is to long name!@dg#d!%#@fd%$f1c@%!#$!"\
+                    "#!@$#@%@#%$@%!#FaSDCARADASFVXT#Q%#$@!$!@$" \
+                    "#FWB THRYu%#$^u6uyj6#$Tga5%@4rFEtwGEQWEFZ" \
+                    "eQEQWvgrtuT(;p8O8olkTU8Uyhdasa213r63634e5",
+            'category':second_cat.id,
+            'unit':liter.id
+        })
+
+        self.assertFalse(form_incorrect.is_valid())
+
+        form_incorrect = ProductForm({
+            'name':'',
+            'category':second_cat.id,
+            'unit':liter.id
+        })
+
+        self.assertFalse(form_incorrect.is_valid())
+
+        form_incorrect = ProductForm({
+            'name':'name',
+            'category':-1,
+            'unit':liter.id
+        })
+
+        self.assertFalse(form_incorrect.is_valid())
+
+        form_incorrect = ProductForm({
+            'name':'name',
+            'category':second_cat.id,
+            'unit':100
+        })
+
+        self.assertFalse(form_incorrect.is_valid())
+
+        second_cat.delete()
+
+        form_incorrect = ProductForm({
+            'name':'Some correct product',
+            'category':second_cat.id,
+            'unit':liter.id
+        })
+
+        self.assertFalse(form_incorrect.is_valid())
+
+        gram.delete()
+
+        form_incorrect = ProductForm({
+            'name':'Some correct product',
+            'category':first_cat.id,
+            'unit':gram.id
+        })
+
+        self.assertFalse(form_incorrect.is_valid())

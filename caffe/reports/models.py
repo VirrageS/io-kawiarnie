64d361985823cdb31871e44c61df0214f7770bee
Marta Rożek <mr360953@students.mimuw.edu.pl>
2016-04-24 17:09:43 +0200

    Corrections in docstrings

--- caffe/reports/models.py
+++ caffe/reports/models.py
@@ -1,12 +1,10 @@
-"""
-    Defines all the classes needed to create a Report from individual
-    FullProducts.
+"""Defines all the classes needed to create a Report from scratch.
 
-    Report: a single document about the state of a cafe.
-    Category: a class of products sharing common characteristics.
-    Product: a single item in a cafe.
-    Unit: a measure of products.
-    FullProduct: a product with its quantity.
+Report: a single document about the state of a cafe.
+Category: a class of products sharing common characteristics.
+Product: a single item in a cafe.
+Unit: a measure of products.
+FullProduct: a product with its quantity.
 """
 
 from django.core.validators import MinValueValidator
@@ -16,32 +14,34 @@ from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible
 class Report(models.Model):
-    """
-        Stores a single report created from selected FullProducts.
-        Date of creation is set automatically.
+    """Stores a single report created from selected FullProducts.
+
+    Date of creation is set automatically.
     """
     created_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return 'Report created: {:%Y-%m-%d %H:%M} '.format(self.created_on)
 
+
 @python_2_unicode_compatible
 class Category(models.Model):
-    """
-        Stores the category of a product, e.g. cake, tea, sandwich.
-        Intended to be created once and then to reuse it in future reports.
+    """Stores the category of a product, e.g. cake, tea, sandwich.
+
+    Intended to be created once and then to reuse it in future reports.
     """
     name = models.CharField(max_length=100, unique=True,)
 
     def __str__(self):
         return '{}'.format(self.name)
 
+
 @python_2_unicode_compatible
 class Product(models.Model):
-    """
-        Stores a specific product, e.g. brownie, earl grey, PB&J sandwich.
-        Intended to be created once and then to reuse it in future reports.
-        Unit specifies how the amount of product is counted.
+    """Stores a specific product, e.g. brownie, earl grey, PB&J sandwich.
+
+    Intended to be created once and then to reuse it in future reports.
+    Unit specifies how the amount of product is counted.
     """
     name = models.CharField(max_length=100, unique=True)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
@@ -50,22 +50,24 @@ class Product(models.Model):
     def __str__(self):
         return '{}'.format(self.name)
 
+
 @python_2_unicode_compatible
 class Unit(models.Model):
-    """
-        Stores a type of unit used to count the amount of products.
-        Intended to be created once and then to reuse it in future reports.
+    """Stores a type of unit used to count the amount of products.
+
+    Intended to be created once and then to reuse it in future reports.
     """
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return '{}'.format(self.name)
 
+
 @python_2_unicode_compatible
 class FullProduct(models.Model):
-    """
-        Stores a product with its quantity.
-        Intended to be used once, only in one report.
+    """Stores a product with its quantity.
+
+    Intended to be used once, only in one report.
     """
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
     amount = models.FloatField(validators=[MinValueValidator(0)])

1d47d39d6c0bd2ff69baa1a56ca83ac207d24792
tknopik <tomasz.knopik@gmail.com>
2016-05-02 15:54:15 +0200

    style changes

--- caffe/stencils/test_views.py
+++ caffe/stencils/test_views.py
@@ -13,16 +13,17 @@ from stencils.forms import StencilForm
 class StencilViewTests(TestCase):
     """Tests views for Stencil model."""
 
-    def compareStencils(self, stencil1, stencil2):
-        """Comapre 2 stencils
+    def compare_stencils(self, stencil1, stencil2):
+        """
+        Comapre 2 stencils
             args: stencil1, stencil2
             return: True is stencils are equal
                     False is are not
         """
 
-        if (stencil1.name != stencil2.name or 
-            stencil1.description != stencil2.description or
-            collections.Counter(stencil1.categories.all()) != 
+        if (stencil1.name != stencil2.name
+            or stencil1.description != stencil2.description
+            or collections.Counter(stencil1.categories.all()) != 
             collections.Counter(stencil2.categories.all())):
             return False
 
@@ -44,33 +45,30 @@ class StencilViewTests(TestCase):
             unit=self.liter
         )
 
-        self.greenTea = Product.objects.create(
+        self.green_tea = Product.objects.create(
             name="Zielona Herbata",
             category=self.tees,
             unit=self.liter
         )
 
-        self.blackCoffe = Product.objects.create(
+        self.black_coffe = Product.objects.create(
             name="Czarna kawa",
             category=self.caffees,
             unit=self.liter
         )
 
-
-        self.toDrink = Stencil.objects.create(
+        self.to_drink = Stencil.objects.create(
             name='Do picia',
             description='picie'
         )
 
+        self.to_drink.categories.add(self.tees, self.juices)
 
-        self.toDrink.categories.add(self.tees, self.juices) 
-    
-
-        self.toEat = Stencil.objects.create(
+        self.to_eat = Stencil.objects.create(
             name='Do jedzenia',
             description='jedzenie'
         )
-        self.toEat.categories.add(self.cakes, self.caffees)
+        self.to_eat.categories.add(self.cakes, self.caffees)
 
     def test_stencil_show_all(self):
         """Check if all stencils view is displayed properly."""
@@ -83,10 +81,10 @@ class StencilViewTests(TestCase):
         self.assertEqual(len(response.context['stencils']), 2)
         stencils = response.context['stencils']
         for stencil in stencils:
-            if stencil.name=='Do picia':
-                self.assertTrue(self.compareStencils(stencil, self.toDrink))
-            elif stencil.name=='Do jedzenia':
-                self.assertTrue(self.compareStencils(stencil, self.toEat))
+            if stencil.name == 'Do picia':
+                self.assertTrue(self.compare_stencils(stencil, self.to_drink))
+            elif stencil.name == 'Do jedzenia':
+                self.assertTrue(self.compare_stencils(stencil, self.to_eat))
             else:
                 self.assertTrue(False)
 
@@ -94,7 +92,7 @@ class StencilViewTests(TestCase):
         """Check if stencil view is displated properly."""
         response = self.client.get(reverse(
             'stencils_show_stencil',
-            args=(self.toDrink.id,)
+            args=(self.to_drink.id,)
         ))
 
         self.assertEqual(response.status_code, 200)
@@ -103,27 +101,27 @@ class StencilViewTests(TestCase):
         stencil = response.context['stencil']
         categories = response.context['categories']
 
-        self.assertTrue(self.compareStencils(stencil, self.toDrink))
+        self.assertTrue(self.compare_stencils(stencil, self.to_drink))
         self.assertEqual(
             collections.Counter(categories),
-            collections.Counter(self.toDrink.categories.all())
+            collections.Counter(self.to_drink.categories.all())
         )
 
     def test_edit_stencil_show(self):
         """Check if edit category view is displayed properly."""
 
         response = self.client.get(
-            reverse('stencils_edit_stencil', args=(self.toDrink.id,))
+            reverse('stencils_edit_stencil', args=(self.to_drink.id,))
         )
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'stencils/edit_stencil.html')
 
         form = response.context['form']
         self.assertIsInstance(form, StencilForm)
-        self.assertEqual(form.instance, self.toDrink)
+        self.assertEqual(form.instance, self.to_drink)
 
         self.assertEqual(len(response.context), 2)
-        
+
     def test_new_stencil(self):
         """Check form to create new stencil."""
 
@@ -151,7 +149,7 @@ class StencilViewTests(TestCase):
         self.assertEqual(Stencil.objects.count(), 3)
         self.assertEqual(response.status_code, 302)
 
-    def test_new_Stencil_post_failure(self):
+    def test_new_stencil_post_failure(self):
         """Check failure of new stencil post request."""
 
         form = {}
@@ -189,46 +187,47 @@ class StencilViewTests(TestCase):
         """Check rendering new stencil report page."""
 
         response = self.client.get(
-            reverse('stencils_new_report', args=(self.toDrink.id,))
+            reverse('stencils_new_report', args=(self.to_drink.id,))
         )
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'stencils/new_report.html')
 
         stencil = response.context['stencil']
-        self.assertTrue(self.compareStencils(stencil, self.toDrink))
+        self.assertTrue(self.compare_stencils(stencil, self.to_drink))
 
-        with self.assertRaises(Exception):
+        with self.assertRaises(NoReverseMatch):
             self.client.get(
                 reverse('stencils_new_report', args=(-1,))
             )
 
-
-    def test_new_stencil_report_post_success(self):
+    def test_stencil_report_post_success(self):
         """Check success of creating new report from stencil."""
+
         post = {}
         post[self.coke.id] = [self.coke.id, 10]
-        post[self.greenTea.id] = [self.greenTea.id, 20]
+        post[self.green_tea.id] = [self.green_tea.id, 20]
 
 
         response = self.client.post(
-            reverse('stencils_new_report', 
-            args=(self.toDrink.id,)), 
+            reverse('stencils_new_report',
+            args=(self.to_drink.id,)),
             post
         )
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(Report.objects.count(), 1)
 
-    def test_new_stencil_report_post_fail(self):
+    def test_stencil_report_post_fail(self):
         """Check failure of creating new report from stencil."""
+
         # wrong amount, should render the same page (200)
         post = {}
         post[self.coke.id] = [self.coke.id, -20]
-        post[self.greenTea.id] = [self.greenTea.id, 20]
+        post[self.green_tea.id] = [self.green_tea.id, 20]
 
         response = self.client.post(
-            reverse('stencils_new_report', 
-            args=(self.toDrink.id,)), 
+            reverse('stencils_new_report',
+            args=(self.to_drink.id,)),
             post
         )
 
@@ -238,9 +237,17 @@ class StencilViewTests(TestCase):
         post = {}
 
         response = self.client.post(
-            reverse('stencils_new_report', 
-            args=(self.toDrink.id,)), 
+            reverse('stencils_new_report',
+            args=(self.to_drink.id,)),
             post
         )
 
         self.assertEqual(response.status_code, 200)
+
+        with self.assertRaises(NoReverseMatch):
+            self.client.post(
+                reverse('stencils_new_report',
+                args=(-1,)),
+                post
+            )
+

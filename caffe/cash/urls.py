383971f37eeb55e197973fc9bf16530c0cb101de
VirrageS <virrages@gmail.com>
2016-05-21 21:19:21 +0200

    Add: initial files for cash reports

---
+++ caffe/cash/urls.py
@@ -0,0 +1,28 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+from .views import (cash_edit_cash_report, cash_edit_company,
+                    cash_edit_expense, cash_navigate, cash_new_cash_report,
+                    cash_new_company, cash_new_expense,
+                    cash_show_all_cash_reports, cash_show_cash_report)
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+
+    url(r'^$', cash_navigate, name='cash_navigate'),
+    url(r'^all/$', cash_show_all_cash_reports, name='show_all_cash_reports'),
+    url(r'^show/(?P<report_id>\d{0,17})/$',
+        cash_show_cash_report, name='show_cash_report'),
+
+    url(r'^new/company/$', cash_new_company, name='new_company'),
+    url(r'^edit/company/(?P<category_id>\d{0,17})/$',
+        cash_edit_company, name='cash_edit_company'),
+
+    url(r'^new/expense/$', cash_new_expense, name='new_expense'),
+    url(r'^edit/expense/(?P<expense_id>\d{0,17})/$',
+        cash_edit_expense, name='edit_expense'),
+
+    url(r'^new/report/$', cash_new_cash_report, name='new_cash_report'),
+    url(r'^edit/report/(?P<report_id>\d{0,17})/$',
+        cash_edit_cash_report, name='edit_cash_report'),
+]
--- caffe/cash/views.py
+++ caffe/cash/views.py
@@ -1,3 +1,171 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Company, Expense, CashReport
+from .forms import CompanyForm, ExpenseForm, CashReportForm
+
+
+def cash_new_company(request):
+    """Show form to create new Company and show existing Companies."""
+
+    elements = []
+    form = CompanyForm()
+
+    if request.POST:
+        form = CompanyForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('cash_navigate'))
+
+    companies = Company.objects.all()
+    for company in companies:
+        elements.append({
+            'edit_href': reverse('cash_edit_company', args=(company.id,)),
+            'id': company.id,
+            'desc': str(company)
+        })
+
+    return render(request, 'cash/new_element.html', {
+        'form': form,
+        'context': {
+            'title': u'Nowa firma'
+        },
+        'elements': elements
+    })
+
+
+def cash_edit_company(request, company_id):
+    """Show form to edit Company.
+
+    Args:
+        company_id (int): Id of Company which is edited.
+    """
+
+    company = get_object_or_404(Company, id=company_id)
+    form = CompanyForm(request.POST or None, instance=company)
+
+    if form.is_valid():
+        form.save()
+        return redirect(reverse('cash_navigate'))
+
+    return render(request, 'cash/edit_element.html', {
+        'form': form,
+        'context': {
+            'title': u'Edytuj firmę',
+            'cancel_href': reverse('cash_new_company')
+        }
+    })
+
+
+def cash_new_expense(request):
+    """Show form to create new Expense and show already existing Expenses."""
+
+    elements = []
+    form = ExpenseForm()
+
+    if request.POST:
+        form = ExpenseForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('cash_navigate'))
+
+    expenses = Expense.objects.all()
+    for expense in expenses:
+        elements.append({
+            'edit_href': reverse('cash_edit_expense', args=(expense.id,)),
+            'id': expense.id,
+            'desc': str(expense)
+        })
+
+    return render(request, 'cash/new_element.html', {
+        'form': form,
+        'context': {
+            'title': "Nowy wydatek"
+        },
+        'elements': elements
+    })
+
+
+def cash_edit_expense(request, expense_id):
+    """Show form to edit Expense.
+
+    Args:
+        expense_id (int): Id of Expense which is edited.
+    """
+
+    expense = get_object_or_404(Expense, id=expense_id)
+    form = ExpenseForm(request.POST or None, instance=expense)
+
+    if form.is_valid():
+        form.save()
+        return redirect(reverse('cash_navigate'))
+
+    return render(request, 'cash/edit_element.html', {
+        'form': form,
+        'context': {
+            'title': 'Edytuj wydatek',
+            'cancel_href': reverse('cash_new_expense')
+        }
+    })
+
+
+def cash_new_cash_report(request):
+    """Show form to create new CashReport and show already existing CashReport."""
+
+    if request.POST:
+        pass
+
+    # get last five reports
+    latest_reports = CashReport.objects.all()[:5]
+
+    return render(request, 'cash/new_report.html', {
+        'title':  'Nowy raport z kasy',
+        'button': 'Dodaj',
+        'reports': latest_reports,
+    })
+
+
+def cash_edit_cash_report(request, report_id):
+    """Show form to edit CashReport.
+
+    Args:
+        report_id (int): Id of CashReport which is edited.
+    """
+
+    report = get_object_or_404(CashReport, id=report_id)
+
+    if request.POST:
+        pass
+
+    return render(request, 'cash/new_report.html', {
+        'title': 'Edytuj raport z kasy',
+        'button': 'Uaktualnij',
+    })
+
+
+def cash_show_cash_report(request, report_id):
+    """Show CashReport with all Expenses.
+
+    Args:
+        report_id (int): Id of CashReport which have to be shown.
+    """
+
+    report = get_object_or_404(CashReport, id=report_id)
+
+    return render(request, 'cash/show.html', {
+        'report': report,
+    })
+
+
+def cash_show_all_cash_reports(request):
+    """Show all existing CashReport."""
+
+    reports = CashReport.objects.all()
+    return render(request, 'cash/all.html', {'reports': reports})
+
+
+def cash_navigate(request):
+    """Show navigation view for CashReport."""
+
+    return render(request, 'home/cash.html')

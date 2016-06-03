a203128f3087b21ebed4abb674c29b3941cc55f6
VirrageS <virrages@gmail.com>
2016-06-03 20:29:57 +0200

    Comment: unnecessary code

--- caffe/home/views.py
+++ caffe/home/views.py
@@ -43,27 +43,27 @@ def caffe_navigate(request):
     })
 
 
-def handler404(request):
-    """Show 404 page whenever 404 error occurs."""
-
-    response = render_to_response(
-        'home/404.html',
-        {},
-        context_instance=RequestContext(request)
-    )
-
-    response.status_code = 404
-    return response
-
-
-def handler500(request):
-    """Show 500 page whenever 500 error occurs."""
-
-    response = render_to_response(
-        'home/500.html',
-        {},
-        context_instance=RequestContext(request)
-    )
-
-    response.status_code = 500
-    return response
+# def handler404(request):
+#     """Show 404 page whenever 404 error occurs."""
+#
+#     response = render_to_response(
+#         'home/404.html',
+#         {},
+#         context_instance=RequestContext(request)
+#     )
+#
+#     response.status_code = 404
+#     return response
+#
+#
+# def handler500(request):
+#     """Show 500 page whenever 500 error occurs."""
+#
+#     response = render_to_response(
+#         'home/500.html',
+#         {},
+#         context_instance=RequestContext(request)
+#     )
+#
+#     response.status_code = 500
+#     return response
